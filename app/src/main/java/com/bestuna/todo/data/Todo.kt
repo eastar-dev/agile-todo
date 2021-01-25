a8f61c76e714b035e871b00b1e6e3406ebb18ec2
eastar <pluto@eastandroid.com>
2021-01-25 10:47:45 +0900

    #3-Data-설계 data 기본값 추가했어요

--- app/src/main/java/com/bestuna/todo/data/Todo.kt
+++ app/src/main/java/com/bestuna/todo/data/Todo.kt
@@ -1,7 +1,9 @@
 package com.bestuna.todo.data
 
+import java.util.*
+
 data class Todo(
-    val id: String,
-    val title: String,
-    val content: String
+    val id: String = UUID.randomUUID().toString(),
+    val title: String = "NoTitle",
+    val content: String = ""
 )
