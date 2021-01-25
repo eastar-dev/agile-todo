eb93783e833aa1a7ff7cc2d10211714c6b6eb119
siwon <siwon9311@gmail.com>
2021-01-25 17:38:24 +0900

    edit 추가

--- app/src/main/java/com/bestuna/todo/TodoDB.kt
+++ app/src/main/java/com/bestuna/todo/TodoDB.kt
@@ -29,12 +29,22 @@ object TodoDB {
         )
     }
 
+
     @Suppress("UNCHECKED_CAST")
     fun get() = PREFERENCES.all
         .values
         .map { it as String }
         .map { JSONArray(it) }
         .map { Todo(it.getString(0)!!, it.getString(1)!!, it.getString(2)!!) }
+
+    @Suppress("UNCHECKED_CAST")
+    fun get(id: String): Todo {
+        val it = PREFERENCES.all[id]
+        return JSONArray(it.toString()).let {
+
+            Todo(it.getString(0)!!, it.getString(1)!!, it.getString(2)!!)
+        }
+    }
 
     fun add(todo: List<Todo>) = todo.forEach { add(it) }
 }
--- app/src/main/java/com/bestuna/todo/UpdateFragment.kt
+++ app/src/main/java/com/bestuna/todo/UpdateFragment.kt
@@ -8,14 +8,21 @@ import androidx.appcompat.widget.Toolbar
 import com.bestuna.todo.data.Todo
 
 class UpdateFragment : CreateFragment() {
-    val todo: Todo = Todo()
+    lateinit var todo: Todo
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        val id = requireArguments().getString(EXTRA_ID)!!
+        todo = TodoDB.get(id)
         view.findViewById<EditText>(R.id.input_title).setText(todo.title)
         view.findViewById<EditText>(R.id.input_content).setText(todo.content)
         view.findViewById<TextView>(R.id.tv_head).text = "Update"
         view.findViewById<TextView>(R.id.btn_add).text = "Update"
 
         (activity as MainActivity).findViewById<Toolbar>(R.id.toolbar).title = "Update"
+    }
+
+    companion object {
+        const val EXTRA_ID = "extra_id"
     }
 }
