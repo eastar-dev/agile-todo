44b0ec3047bfb07eb370216831572102a7617f3c
jdm <bblbbltyu@naver.com>
2021-01-25 11:32:03 +0900

    viewmodel 추가 및 어댑터 추가

---
+++ app/src/main/java/com/bestuna/todo/DisplayViewModel.kt
@@ -0,0 +1,9 @@
+package com.bestuna.todo
+
+import androidx.lifecycle.ViewModel
+
+class DisplayViewModel: ViewModel() {
+
+
+
+}
--- app/src/main/java/com/bestuna/todo/MainActivity.kt
+++ app/src/main/java/com/bestuna/todo/MainActivity.kt
@@ -1,17 +1,33 @@
 package com.bestuna.todo
 
+import android.content.Context
 import android.os.Bundle
+import android.view.*
+import android.widget.TextView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import android.view.Menu
-import android.view.MenuItem
+import androidx.activity.viewModels
+import androidx.databinding.DataBindingUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.bestuna.todo.data.Todo
+import com.bestuna.todo.databinding.ActivityMainBinding
+import java.util.zip.Inflater
 
 class MainActivity : AppCompatActivity() {
 
+    var adapter :TodoDisplayAdapter? = null
+    lateinit var bb : ActivityMainBinding
+    val vm : DisplayViewModel by viewModels()
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        bb = DataBindingUtil.setContentView(this,R.layout.activity_main)
+        bb.vm = vm
+
+        adapter = TodoDisplayAdapter(this,mutableListOf(Todo("1","Title Test","Content Test")))
+        bb.todoList.adapter = adapter
 
         findViewById<FloatingActionButton>(R.id.fab).setOnClickListener { view ->
             Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
@@ -33,5 +49,32 @@ class MainActivity : AppCompatActivity() {
             R.id.action_settings -> true
             else -> super.onOptionsItemSelected(item)
         }
+    }
+
+
+    inner class TodoDisplayAdapter(context: Context, list:MutableList<Todo>) : RecyclerView.Adapter<TodoDisplayAdapter.ToDoDisplayViewHolder>() {
+        var DisplayList = list
+        var context = context
+
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ToDoDisplayViewHolder {
+            val view = LayoutInflater.from(context).inflate(R.layout.displaylist_item,parent,false)
+            return ToDoDisplayViewHolder(view)
+        }
+
+        override fun onBindViewHolder(holder: ToDoDisplayViewHolder, position: Int) {
+            holder.id.text = DisplayList[position].id
+            holder.title.text = DisplayList[position].title
+            holder.content.text = DisplayList[position].content
+        }
+
+        override fun getItemCount() = DisplayList.size
+
+        inner class ToDoDisplayViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+            val title = view.findViewById<TextView>(R.id.title)
+            val content = view.findViewById<TextView>(R.id.content)
+            val id = view.findViewById<TextView>(R.id.id)
+        }
+
+
     }
 }
